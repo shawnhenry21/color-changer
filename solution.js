92ee01cfe990f6002aaeae195ec945bb716eb122
shawnhenry21 <smand4275@wth.org>
2016-04-22 13:51:32 +0000

    color changer

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
+var colors = ["red", "blue", "yellow", "black"];
 
+function changeColor(){
+    var randomIndex =Math.floor(Math.random() *colors.length);
+    
+    $("body").css("background-color", colors[randomIndex]);
+}
+
+function addColor() {
+    var newColor = $("#newColor").val();
+    colors.push(newColor);
+    $("body").css("background-color" , newColor);
+}
+
+$( document).ready(function() {
+    $("#change").click(changeColor);
+    $("#add").click(addColor);
+});
